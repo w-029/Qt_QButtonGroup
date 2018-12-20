f4fbbd49e3efc66b962937de73829fe2e7daec1e
w-029 <w-029@163.com>
2018-12-20 23:23:17 +0800

    GUI: start testing & set result

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,14 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->checkBox_0->setVisible(false);
+    ui->cb_assit->setVisible(false);
 
     buttonGroup = new QButtonGroup;
-    buttonGroup->addButton(ui->checkBox_0, -1);
-    buttonGroup->addButton(ui->checkBox, 0);
-    buttonGroup->addButton(ui->checkBox_1, 1);
+    buttonGroup->addButton(ui->cb_assit, -1);
+    buttonGroup->addButton(ui->cb_testing, 0);
+    buttonGroup->addButton(ui->cb_result_ok, 1);
+    buttonGroup->addButton(ui->cb_result_ng, 2);
 
-    timerId = this->startTimer(500);
+    connect(ui->pb_test_exe, SIGNAL(clicked()), this, SLOT(on_startTwinkle()));
+    connect(ui->cb_testing, SIGNAL(clicked()), this, SLOT(on_startTwinkle()));
+    connect(ui->cb_result_ok, SIGNAL(clicked()), this, SLOT(on_endTwinkle()));
+    connect(ui->cb_result_ng, SIGNAL(clicked()), this, SLOT(on_endTwinkle()));
 }
 
 MainWindow::~MainWindow()
@@ -21,18 +25,35 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_startTwinkle()
+{
+    bTwinkle = true;
+    ui->cb_testing->setChecked(true);
+    timerId = this->startTimer(800);
+}
+
+void MainWindow::on_endTwinkle()
+{
+    bTwinkle = false;
+}
+
 void MainWindow::timerEvent(QTimerEvent* event)
 {
-    if (event->timerId() == timerId) {
-        if (ui->checkBox->isChecked()) {
-            /* this is not take effect */
-//            ui->checkBox->setChecked(false);
+    if (bTwinkle && event->timerId() == timerId) {
+        twinkle(ui->cb_testing);
+    }
+}
 
-            /* this is pivotal */
-            ui->checkBox_0->setChecked(true);
-        }
-        else {
-            ui->checkBox->setChecked(true);
-        }
+void MainWindow::twinkle(QCheckBox* cbTwinkle)
+{
+    if (ui->cb_testing->isChecked()) {
+        /* this will not take effect */
+//        ui->cb_testing->setChecked(false);
+
+        /* this is pivotal */
+        ui->cb_assit->setChecked(true);
+    }
+    else {
+        cbTwinkle->setChecked(true);
     }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QButtonGroup>
+#include <QCheckBox>
 
 namespace Ui {
 class MainWindow;
@@ -16,13 +17,19 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void on_startTwinkle();
+    void on_endTwinkle();
+
 private:
     void timerEvent(QTimerEvent* event);
+    void twinkle(QCheckBox* cb);
 
 private:
     Ui::MainWindow *ui;
     QButtonGroup* buttonGroup;
     int timerId;
+    bool bTwinkle;
 };
 
 #endif // MAINWINDOW_H
